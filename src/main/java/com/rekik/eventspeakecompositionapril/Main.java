08cce7baf303fed81f3cd0a10fa1fcbfa8e099c3
rekikhaile <rekikhaile@gmail.com>
2018-04-11 17:10:26 -0400

    working on past and upcoming events

--- src/main/java/com/rekik/eventspeakecompositionapril/Main.java
+++ src/main/java/com/rekik/eventspeakecompositionapril/Main.java
@@ -1,10 +1,16 @@
 package com.rekik.eventspeakecompositionapril;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
 
         Scanner reader = new Scanner(System.in);
         boolean eventDone = true;
@@ -18,6 +24,18 @@ public class Main {
         Speaker aSpeaker;
         String eventName="";
         ArrayList<Event> events = new ArrayList<>();
+        ArrayList<Event> pastEvents = new ArrayList<>();
+        ArrayList<Event> upcomingEvents = new ArrayList<>();
+
+        LocalDate rightNow= LocalDate.now();
+        LocalDate anyDay = null;
+        LocalDate endDate=null;
+        DateTimeFormatter longFormat = DateTimeFormatter.ofPattern("dd MMMM yyyy");
+        DateTimeFormatter dTF = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        System.out.println("Today is: "+rightNow.format(dTF) + " welcome!");
+
+
 
         do {
 
@@ -25,6 +43,9 @@ public class Main {
             System.out.println("2. Add a speaker to an event  ");
             System.out.println("3. Show all the speakers for all events ");
             System.out.println("4. Show all the speakers for a given event (selected by the user)");
+            System.out.println("5. Show upcoming events ");
+            System.out.println("6. Show past events ");
+            System.out.println("7. Enter a day to search for past and upcoming events ");
 
             System.out.println("Enter your choice from the menu");
             choice=reader.nextInt();
@@ -56,6 +77,120 @@ public class Main {
                 } while (eventDone);
 
             }
+
+
+            else if (choice == 7){
+
+                System.out.println("Enter a date: ");
+                String myDate = reader.nextLine();
+
+                anyDay = LocalDate.parse(myDate, dTF);
+
+
+                if(!events.isEmpty()) {
+                    for (Event eachEvent : events
+                            ) {
+                        endDate = LocalDate.parse(eachEvent.getEndDate(),dTF);
+                        System.out.println(endDate.format(longFormat));
+
+                        System.out.println(endDate.format(dTF).compareTo(anyDay.format(dTF)));
+
+                        if(endDate.format(dTF).compareTo(anyDay.format(dTF)) > 0){
+
+                            //System.out.println("Added to the upcoming events");
+                            upcomingEvents.add(eachEvent);
+                        }
+                        else{
+                            //System.out.println("Added to the past events");
+                            pastEvents.add(eachEvent);
+                        }
+
+
+                    }
+
+                    System.out.println("The upcoming events are the following");
+
+                        if(!upcomingEvents.isEmpty()) {
+                            for (Event past : upcomingEvents
+                                    ) {
+
+                                System.out.println(past.getTitle());
+
+
+                            }
+                        }
+                        else System.out.println("There are no upcoming events");
+
+                    System.out.println("The past events are the following");
+
+                        if(!pastEvents.isEmpty()) {
+                            for (Event past : pastEvents
+                                    ) {
+
+                                System.out.println(past.getTitle());
+                            }
+                        }
+                        else System.out.println("There are no past events");
+
+
+
+
+
+
+
+
+                }
+
+                else System.out.println("There are no events");
+
+
+
+
+
+                   /* boolean isBefore = rightNow.isBefore(endDate);
+
+                    if(isBefore)
+                        System.out.println("I am rocking");*/
+
+
+            }
+
+/*
+            do{
+
+                System.out.println("Enter a date: ");
+                String myDate = reader.nextLine();
+
+                anyDay = LocalDate.parse(myDate, dTF);
+
+                //treating endDate as a date
+                //endDate = LocalDate.parse(anEvent.getEndDate(),dTF);
+                //System.out.println(endDate.format(longFormat));
+
+                if(!events.isEmpty()) {
+                    for (Event eachEvent : events
+                            ) {
+                        endDate = LocalDate.parse(eachEvent.getEndDate(),dTF);
+                        System.out.println(endDate.format(longFormat));
+                    }
+                }
+
+                else System.out.println("There are no events");
+
+
+
+                if(endDate.format(dTF).compareTo(anyDay.format(dTF)) > 0){
+                    System.out.println("Added to the upcoming events");
+                    upcomingEvents.add(anEvent);
+                }
+                else{
+                    System.out.println("Added to the past events");
+                    pastEvents.add(anEvent);
+                }
+
+            }while(true); */
+
+
 
             ////////////////////////////////
             else if (choice == 2) {
@@ -122,17 +257,19 @@ public class Main {
 
             else if (choice == 3){
 
-                for (Event eachEvent : events
-                        ) {
-                   System.out.println("Event :" +eachEvent.getTitle());
-                    if(!eachEvent.getSpeakers().isEmpty()) {
-                        for (Speaker eachSpeaker : eachEvent.getSpeakers()) {
-                            System.out.println("Speaker: " + eachSpeaker.getName());
-                        }
-                    }
-                    else System.out.println("No speakers for this event");
+                if(!events.isEmpty()) {
+                    for (Event eachEvent : events
+                            ) {
+                        System.out.println("Event :" + eachEvent.getTitle());
+                        if (!eachEvent.getSpeakers().isEmpty()) {
+                            for (Speaker eachSpeaker : eachEvent.getSpeakers()) {
+                                System.out.println("Speaker: " + eachSpeaker.getName());
+                            }
+                        } else System.out.println("No speakers for this event");
 
+                    }
                 }
+                else System.out.println("There are no events");
         }
 
         /////////////////////////////////
@@ -168,6 +305,33 @@ public class Main {
                 }else System.out.println("there are no events");
                     //events.set(k, tempEvent);
                 }
+
+
+
+            else if(choice == 5){
+                if(!upcomingEvents.isEmpty()) {
+                    for (Event past : upcomingEvents
+                            ) {
+
+                        System.out.println(past.getTitle());
+
+
+                    }
+                }
+                else System.out.println("There are no upcoming events");
+            }
+
+            else if(choice == 6){
+                if(!pastEvents.isEmpty()) {
+                    for (Event past : pastEvents
+                            ) {
+
+                        System.out.println(past.getTitle());
+                    }
+                }
+                else System.out.println("There are no past events");
+            }
+
 
 
             System.out.println("Do you want to see the menu again");
